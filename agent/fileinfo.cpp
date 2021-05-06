deec17fef16d7eff31fceee420a17e0aa932d187
ChenYing Kuo <evshary@gmail.com>
2021-05-06 14:09:40 +0800

    Support export for file transfer.

--- agent/fileinfo.cpp
+++ agent/fileinfo.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int import_testfile(char *file_buf, int file_len) {
     printf("import test file.\n");
     return 1234;
+}
+
+int export_testfile(char *file_buf, int file_len) {
+    printf("Modify export file\n");
+    file_buf[2] = 'a';
+    file_buf[3] = 't';
+    return 2345;
 }
--- agent/fileinfo.hpp
+++ agent/fileinfo.hpp
@@ -2,5 +2,6 @@
 #define _FILEINFO_HPP_
 
 int import_testfile(char *file_buf, int file_len);
+int export_testfile(char *file_buf, int file_len);
 
 #endif /*_FILEINFO_HPP_*/
