6b263098fb69000f5bb84364cb19beae0419eca6
ChenYing Kuo <evshary@gmail.com>
2021-05-18 14:02:05 +0800

    Check the file content.

--- agent/fileinfo.cpp
+++ agent/fileinfo.cpp
@@ -2,13 +2,20 @@
 #include <string.h>
 
 int import_testfile(char *file_buf, int file_len) {
-    printf("import test file.\n");
-    return 1234;
+    printf("import: length of file=%d\n", file_len);
+    return file_len;
 }
 
 int export_testfile(char *file_buf, int file_len) {
+    int ret = 0;
+
     printf("Modify export file\n");
-    file_buf[2] = 'a';
-    file_buf[3] = 't';
-    return 2345;
+    if (file_buf != NULL && file_len > 4) {
+        file_buf[2] = 'a';
+        file_buf[3] = 't';
+    } else {
+        ret = -1;
+    }
+
+    return ret;
 }
