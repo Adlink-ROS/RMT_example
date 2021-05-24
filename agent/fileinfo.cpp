f695b820761e9bbc83c2e65d202a232f800a22d9
ChenYing Kuo <evshary@gmail.com>
2021-05-24 19:39:48 +0800

    Modify the file transfer callback.

--- agent/fileinfo.cpp
+++ agent/fileinfo.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int import_testfile(char *file_buf, int file_len) {
+int import_testfile(char *filename, char *file_buf, int file_len) {
+    printf("import filename: %s\n", filename);
     printf("import: length of file=%d\n", file_len);
     return file_len;
 }
 
-int export_testfile(char *file_buf, int file_len) {
+int export_testfile(char *filename, char *file_buf, int file_len) {
     int ret = 0;
 
+    printf("export filename: %s\n", filename);
     printf("Modify export file\n");
     if (file_buf != NULL && file_len > 4) {
         file_buf[2] = 'a';
