a3cefbc1100b24b7be2db633ad667681b80cd9e6
ChenYing Kuo <evshary@gmail.com>
2021-05-31 15:13:19 +0800

    Use CPP instead of C.

--- tools/uncrustify.sh
+++ tools/uncrustify.sh
@@ -5,7 +5,7 @@ mycfg=linux_c.cfg
 ret=0
 for file in "$@"
 do
-    uncrustify -c ${mycfg} -l c --check ${file} > /dev/null 2>&1
+    uncrustify -c ${mycfg} -l cpp --check ${file} > /dev/null 2>&1
     if [ $? -ne 0 ]; then
     	echo "---------- uncrustify for ${file} ----------"
         uncrustify -c ${mycfg} -l c -f ${file} -o output.tmp
