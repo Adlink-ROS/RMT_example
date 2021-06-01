533c1efd48a6c619f990947e6e432ce019263e6b
ChenYing Kuo <evshary@gmail.com>
2021-06-01 11:26:03 +0800

    Able to build without ROS.

--- agent/datainfo.hpp
+++ agent/datainfo.hpp
@@ -1,10 +1,12 @@
 #ifndef _DATAINFO_HPP_
 #define _DATAINFO_HPP_
 
+#ifdef SUPPORT_ROS
 #include <rclcpp/rclcpp.hpp>
+extern rclcpp::Node::SharedPtr node;
+#endif /*SUPPORT_ROS*/
 
 extern char interface[50];
-extern rclcpp::Node::SharedPtr node;
 
 int get_cpu(char *payload);
 int get_ram(char *payload);
@@ -13,12 +15,14 @@ int set_hostname(char *payload);
 int get_wifi(char *payload);
 int set_wifi(char *payload);
 int set_locate(char *payload);
+#ifdef SUPPORT_ROS
 int get_domain_id(char *payload);
 int set_domain_id(char *payload);
 int get_node_list(char *payload);
 int get_task_list(char *payload);
 int get_task_mode(char *payload);
 int set_task_mode(char *payload);
+#endif /*SUPPORT_ROS*/
 
 void add_wifi_connection(NMClient *client);
 #ifdef SUPPORT_NLIB
